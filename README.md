6378bd835150b9fbcfeffc7d63f5a4a50121625c
jucaripo <jucaripo@hotmail.com>
2019-12-01 23:02:03 -0600

    Material Design Color System Demo

---
+++ README.md
@@ -0,0 +1,6 @@
+Xamarin Forms Material Design  Color System
+
+
+Repositorio con el ejemplo de como implementar  los colores de Material Design en proyectos Xamarin.
+
+Perimitiendo tener un mejor control sobre los colores  y las actualizaciones de los proyectos.
---
+++ metaldesign/AppShell.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace metaldesign
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Application.Current.Resources["NavigationPrimary"] = Application.Current.Resources["MdPrimaryColor"];
+
+
+        }
+    }
+}
